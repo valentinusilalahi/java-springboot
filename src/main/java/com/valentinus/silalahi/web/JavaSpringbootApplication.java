a50a90246708fd07e37982beb2fd94cde482ba20
valentinusilalahi <valentinus.silalahi@gmail.com>
2016-09-22 03:13:37 +0700

    struktur project menggunakan maven

---
+++ src/main/java/com/valentinus/silalahi/web/JavaSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.valentinus.silalahi.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootApplication.class, args);
+	}
+}
